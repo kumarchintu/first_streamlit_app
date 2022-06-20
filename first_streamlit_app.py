6c4b4042af69615e803ef4466944de5bb85b2aa2
Abhishek Kumar <abhi.mca.pu@gmail.com>
2022-06-20 09:52:41 +0530

    Added more code
    Added menu and available options

--- first_streamlit_app.py
+++ first_streamlit_app.py
@@ -1,4 +1,7 @@
 import streamlit
 
 streamlit.title('My parents new healthy diner')
-
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega3 and Blueberry Oatmeal')
+streamlit.text('Kale, Spinch & Rocket Smoothie')
+streamlit.text('Hard-boiled free-range egg')
