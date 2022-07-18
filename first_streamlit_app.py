8c3e9a3a32fccac3348fdc0ea4dccbdaf8384d5e
Abhishek Kumar <abhi.mca.pu@gmail.com>
2022-07-18 19:58:32 +0530

    Update first_streamlit_app.py

--- first_streamlit_app.py
+++ first_streamlit_app.py
@@ -7,4 +7,8 @@ streamlit.text('Omega3 and Blueberry Oatmeal')
 streamlit.text('Kale, Spinch & Rocket Smoothie')
 streamlit.text('Hard-boiled free-range egg')
 my_fruit_list=pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+#Let's put a pick list here so they can pick the fruit they want to include
+streamlit.multiselect("Pick some fruits:",list(my_fruit_list.index))
+
+#display  the table on the page
 streamlit.dataframe(my_fruit_list)
