bd5457c4f379170bfba33e0ff13ac6ca8f6dafc5
Abhishek Kumar <abhi.mca.pu@gmail.com>
2022-07-18 19:54:52 +0530

    Update first_streamlit_app.py

--- first_streamlit_app.py
+++ first_streamlit_app.py
@@ -1,7 +1,10 @@
 import streamlit
+import pandas
 
 streamlit.title('My parents new healthy diner')
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega3 and Blueberry Oatmeal')
 streamlit.text('Kale, Spinch & Rocket Smoothie')
 streamlit.text('Hard-boiled free-range egg')
+my_fruit_list=pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
